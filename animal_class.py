53dbaec554b82d120bd475a281427b19f6981f08
ElviraSi <Elvi.si@yandex.ru>
2020-08-16 07:18:51 +0300

    Update animal_class.py

--- animal_class.py
+++ animal_class.py
@@ -10,17 +10,13 @@ class Animal:
         self.action = 'feed'
         print(f'I {self.action} {self.nickname}.')
 
-    def __add__(self, weight):
-        total_weight = 0
-        total_weight += self.weight
-        return total_weight
-
+    #дополнительная возможность сравнить вес двух животных
     def __gt__(self, other):
         return self.weight > other.weight
 
 
 class Bird(Animal):
-    def collect(self):
+    def collect_eggs(self):
         self.action = 'collect eggs'
         print(f'I {self.action} {self.nickname}.')
 
@@ -43,28 +39,26 @@ class Chiken(Bird):
         print(f'Chiken {self.nickname} make sounds: {self.voice}.')
 
 
-class Cow(Animal):
-    def milk(self):
-        self.action = 'milk'
+class Cattle(Animal):
+    def get_milk(self):
+        self.action = 'get milk'
         print(f'I {self.action} cow {self.nickname}.')
 
+
+class Cow(Cattle):
     def make_sounds(self):
         self.voice = 'moooooo'
         print(f'Cow {self.nickname} make sounds: {self.voice}.')
 
 
-class Goat(Animal):
-    def milk(self):
-        self.action = 'milk'
-        print(f'I {self.action} goat {self.nickname}.')
-
+class Goat(Cattle):
     def make_sounds(self):
         self.voice = 'baaaaa'
         print(f'Goat {self.nickname} make sounds: {self.voice}.')
 
 
 class Sheep(Animal):
-    def shear(self):
+    def get_wool(self):
         self.action = 'shear'
         print(f'I {self.action} sheep {self.nickname}.')
 
@@ -72,14 +66,28 @@ class Sheep(Animal):
         self.voice = 'baaa'
         print(f'Sheep {self.nickname} make sounds: {self.voice}.')
 
-#example
-goat = Goat('Horns', 1000)
-goat.milk()
-goat.make_sounds()
+#создание списка с экземплярами класса
+Animal = []
+Animal.append(Goose('Серый', 10))
+Animal.append(Goose('Белый', 7))
+Animal.append(Cow('Манька', 150))
+Animal.append(Sheep('Барашек', 48))
+Animal.append(Sheep('Кудрявый', 52))
+Animal.append(Chiken('Ко-Ко', 2))
+Animal.append(Chiken('Кукареку', 1))
+Animal.append(Goat('Рога', 34))
+Animal.append(Goat('Копыта', 28))
+Animal.append(Duck('Кряква', 4))
 
-chiken = Chiken('Co-Co', 120)
-chiken.feed()
-chiken.make_sounds()
+#расчет общего веса всех животных
+weight_list = [cl.weight for cl in Animal]
+total_weight = 0
+for i in weight_list:
+    total_weight += i
+print(f'Общий вес всех животных составляет: {total_weight} кг.')
 
-print(goat + chiken)
-print(goat > chiken)
+#нахождение животного с максимальным весом
+animal_list = [cl.nickname for cl in Animal]
+animal_dict = dict(zip(animal_list, weight_list))
+max_weight = max(animal_dict, key=animal_dict.get)
+print(f'Максимальный вес имеет {max_weight}.')
