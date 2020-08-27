8aaf36aa15c861c14ae3d52fb103f491f7c4ed05
ElviraSi <Elvi.si@yandex.ru>
2020-08-27 08:59:45 +0300

    Update parse_xml.py

--- HW_Parse/parse_xml.py
+++ HW_Parse/parse_xml.py
@@ -1,31 +1,20 @@
 import xml.etree.ElementTree as ET
+import collections
 
 parser = ET.XMLParser(encoding="utf-8")
 tree = ET.parse("newsafr.xml", parser)
 root = tree.getroot()
 
-news_list = root.findall("channel/item")
+news_list = root.findall("channel/item/description")
 
 words_list = []
 for new in news_list:
-    descipt = new.find("description").text
+    descipt = new.text
     words_list.extend(descipt.split())
-
+words_list = [x.lower() for x in words_list]
 words_list_upd = []
 for i in words_list:
-    if len(i) >= 6:
+    if len(i) > 6:
         words_list_upd.append(i)
-
-words_dict = {}.fromkeys(words_list_upd, 0)
-for word in words_list_upd:
-    words_dict[word] += 1
-words_dict_upd = {}
-
-for key, value in words_dict.items():
-    words_dict_upd[value] = key
-
-for k in sorted(words_dict_upd.keys(), reverse=True)[:10]:
-    if k % 10 == 2 or k % 10 == 3 or k % 10 == 4:
-        print(f'Слово "{words_dict_upd[k]}" повторяется {k} раза.')
-    else:
-        print(f'Слово "{words_dict_upd[k]}" повторяется {k} раз.')
+count_w = collections.Counter(words_list_upd).most_common(10)
+print(count_w)
